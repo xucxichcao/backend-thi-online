a06695147cab5d26e82a2f63cce7e59726ca34d9
Cao Trọng Nghĩa <thekrualfox@gmail.com>
2021-10-22 15:34:06 +0700

    Add other models

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
-from django.db.models.fields.files import FileField
+from django.db.models import constraints
+from django.db.models.deletion import CASCADE
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import CustomUserManager
@@ -73,9 +74,43 @@ class GiangVien(models.Model):
     date_of_birth = models.DateField(blank=True, null=True)
 
 
+class DeThi(models.Model):
+    soLuongCauHoi = models.PositiveSmallIntegerField()
+
+
+class ChiTietDeThi(models.Model):
+    deThi = models.ForeignKey(DeThi, on_delete=models.CASCADE)
+    noiDung = models.TextField()
+    dapAn = models.PositiveSmallIntegerField()
+
+
+class PhongThi(models.Model):
+    tenPhongThi = models.CharField(blank=True, max_length=200)
+    siSo = models.PositiveSmallIntegerField()
+    giangVien = models.ForeignKey(GiangVien, on_delete=models.CASCADE)
+    danhSach = models.FileField(upload_to="danhSachPhongThi/")
+    deThi = models.ForeignKey(DeThi, on_delete=models.CASCADE)
+    thoiGianLamBai = models.PositiveSmallIntegerField()
+    thoiGianThi = models.DateTimeField()
+    namHoc = models.CharField(max_length=11)
+    hocKi = models.PositiveIntegerField()
+
+
+class DiemThi(models.Model):
+    sinhVien = models.ForeignKey(SinhVien, on_delete=models.CASCADE)
+    phongThi = models.ForeignKey(PhongThi, on_delete=CASCADE)
+    diem = models.DecimalField(decimal_places=2, max_digits=4)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['sinhVien', 'phongThi'], name="unique Sv PhongThi")
+        ]
+
+
 class accountBulkCreate(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    csv_file = FileField(upload_to="bulkAccount/")
+    csv_file = models.FileField(upload_to="bulkAccount/")
     type = models.CharField(max_length=1, choices=(
         ('T', 'Giảng viên'), ('S', 'Sinh viên')), blank=False, default='S')
 
