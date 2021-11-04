3435edc1137bca2c2ab26bfc60dc8997f1784fbf
Cao Trọng Nghĩa <thekrualfox@gmail.com>
2021-11-04 23:06:55 +0700

    fix some url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import AccountBulkCreateViewSet
 
 router = DefaultRouter()
-router.register("bulk-create", AccountBulkCreateViewSet, basename="bulkCreate")
+router.register("school/create-user", AccountBulkCreateViewSet,
+                basename="bulkCreate")
 
 account_urlpatterns = [url("api/", include(router.urls))]
